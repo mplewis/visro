032ac5500009e3711c7699e52ef20040557bea93
Matthew Lewis <matt@mplewis.com>
2016-05-13 23:40:19 -0500

    html: relink js/css, js: move map to points in order

--- visro.js
+++ visro.js
@@ -38,6 +38,13 @@ function plotLine() {
       'line-width': 8
     }
   })
+
+  var pointNum = 0
+  setInterval(function() {
+    point = polyCoords[pointNum]
+    map.panTo(point)
+    pointNum++
+  }, 250)
 }
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibXBsZXdpcyIsImEiOiJpRFNqOTBJIn0.Q1ZmuYl5o3u72T4VFQ4kqQ'
@@ -45,13 +52,14 @@ var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/outdoors-v9',
 })
-map.setZoom(10)
-map.setCenter([-88.9114786,47.9958654])
 
-map.on('load', function () {
+map.on('load', function() {
   styleReady = true
   plotLine()
 })
+
+map.setZoom(10)
+map.setCenter([-88.9114786,47.9958654])
 
 var req = new XMLHttpRequest()
 req.addEventListener('load', reqListener)
